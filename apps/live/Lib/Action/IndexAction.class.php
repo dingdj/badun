18079609edd8e3490a411f07d3dfe099291a0feb
施思宇 <3424287697@qq.com>
2017-12-18 17:10:02 +0800

    回调地址修改

--- apps/live/Lib/Action/IndexAction.class.php
+++ apps/live/Lib/Action/IndexAction.class.php
@@ -1085,7 +1085,7 @@ class IndexAction extends CommonAction {
             $url = "{$res['studentJoinUrl']}&autoLogin=true&username={$user_info['uname']}&password={$res['studentClientToken']}";
         }
         if (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger')) {
-            model('WxPay')->getWxUserInfo($_GET['code'], SITE_URL . '/my/watch/'.$id.'.html');
+            model('WxPay')->getWxUserInfo($_GET['code'], SITE_URL . '/live/watch/'.$id.'.html');
         }
         if($this->is_wap && strpos( $_SERVER['HTTP_USER_AGENT'], 'MicroMessenger')){
             $this->assign('is_wx',true);
